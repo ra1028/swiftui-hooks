4a78f0b5572460f5f1a5c23d482364cbdc399133
Ryo Aoyama <r.fe51028.r@gmail.com>
2021-07-24 04:47:12 +0900

    Restructure

--- Package.swift
+++ Package.swift
@@ -4,12 +4,21 @@ import PackageDescription
 
 let package = Package(
     name: "Hooks",
-    platforms: [.iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(name: "Hooks", targets: ["Hooks"]),
     ],
     targets: [
-        .target(name: "Hooks")
+        .target(name: "Hooks"),
+        .testTarget(
+            name: "HooksTests",
+            dependencies: ["Hooks"]
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )

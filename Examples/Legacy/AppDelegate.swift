354bca5cf30f19dfdc7ab059d818355f27e599e0
Ryo Aoyama <r.fe51028.r@gmail.com>
2021-08-08 04:45:36 +0900

    Refactor todo app example

--- Examples/Legacy/AppDelegate.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-@main
-final class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-}

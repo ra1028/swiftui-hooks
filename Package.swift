8042b00667089d827b70cc7a42d78eb1c330749e
Ryo Aoyama <r.fe51028.r@gmail.com>
2022-04-12 21:30:18 +0900

    Update README

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import Foundation
 import PackageDescription
 
 let package = Package(
-    name: "Hooks",
+    name: "SwiftUI-Hooks",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),

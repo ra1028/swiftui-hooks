6a0ee32228e6527c4b9235f77d325feedb1b6c85
Ryo Aoyama <r.fe51028.r@gmail.com>
2022-04-12 23:01:24 +0900

    Replace all upper case repo names with lower case letters

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import Foundation
 import PackageDescription
 
 let package = Package(
-    name: "SwiftUI-Hooks",
+    name: "swiftui-hooks",
     platforms: [
         .iOS(.v13),
         .macOS(.v10_15),
